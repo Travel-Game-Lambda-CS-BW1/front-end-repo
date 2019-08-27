169c180379535a3aa3b9d01248f4fc2dc1568262
Timothy Shores <timothymshores@gmail.com>
2019-08-27 15:30:00 -0500

    Set localStorage and App.js state to include key from /api/registration/ endpoint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import useForm from "react-hook-form";
 
 import "./App.css";
 
 const App = () => {
-	const { register, handleSubmit, watch, errors } = useForm();
+	const { register, handleSubmit, errors } = useForm();
+	const [key, setKey] = useState();
+
+	useEffect(() => {
+		setKey(localStorage.getItem("key"));
+		console.log("App.js useEffect, setKey to", localStorage.getItem("key"));
+	}, [key]);
 
 	const onSubmit = data => {
-		console.log(data);
+		console.log("user input", data);
+
+		axios
+			.post("http://lambda-mud-test.herokuapp.com/api/registration/", {
+				username: data.username,
+				password1: data.password,
+				password2: data.password
+			})
+			.then(res => {
+				console.log(res.data);
+				localStorage.setItem("key", res.data.key);
+			})
+			.catch(err => console.log(err));
 	};
 
-	console.log(watch("username"));
-
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input name="username" defaultValue="test" ref={register} />
-			<input name="password" ref={register({ required: true })} />
-			{errors.usernameRequired && <span>This field is required</span>}
-			<input type="submit" />
-		</form>
+		<div className="App">
+			<h1>LambdaMUD</h1>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input name="username" defaultValue="test" ref={register} />
+				<br />
+				<input name="password" ref={register({ required: true })} />
+				{errors.usernameRequired && <span>This field is required</span>}
+				<br />
+				<input type="submit" />
+			</form>
+		</div>
 	);
 };
 
