1c0a329e11325c3cf85e56ef91d6d1dc535ac629
Timothy Shores <timothymshores@gmail.com>
2019-08-27 12:49:26 -0500

    Install react-hook-form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-      <div className="App">
-          <h1>LambdaMUD</h1>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<h1>LambdaMUD</h1>
+			<button onClick={() => console.log()}>Register</button>
+		</div>
+	);
+};
 
 export default App;
