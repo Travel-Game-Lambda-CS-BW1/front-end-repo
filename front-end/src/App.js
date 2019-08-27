eaf16bf0793de867e6485372891003f223330904
Timothy Shores <timothymshores@gmail.com>
2019-08-27 13:00:33 -0500

    Install axios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import useForm from "react-hook-form";
+
 import "./App.css";
 
 const App = () => {
+	const { register, handleSubmit, watch, errors } = useForm();
+
+	const onSubmit = data => {
+		console.log(data);
+	};
+
+	console.log(watch("username"));
+
 	return (
-		<div className="App">
-			<h1>LambdaMUD</h1>
-			<button onClick={() => console.log()}>Register</button>
-		</div>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input name="username" defaultValue="test" ref={register} />
+			<input name="password" ref={register({ required: true })} />
+			{errors.usernameRequired && <span>This field is required</span>}
+			<input type="submit" />
+		</form>
 	);
 };
 
