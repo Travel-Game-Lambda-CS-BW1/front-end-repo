b58963510eeb03bdc0175dcdaa60c1a28d592262
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-08-29 07:19:13 -0500

    added second field for password

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -3,34 +3,36 @@ import axios from "axios";
 import useForm from "react-hook-form";
 
 const Register = () => {
-	const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm();
 
-	const onSubmit = data => {
-		console.log("user input", data);
+  const onSubmit = data => {
+    console.log("user input", data);
 
-		axios
-			.post("https://travel-game-python.herokuapp.com/api/registration/", {
-				username: data.username,
-				password1: data.password,
-				password2: data.password
-			})
-			.then(res => {
-				console.log(res.data);
-				localStorage.setItem("key", res.data.key);
-			})
-			.catch(err => console.log(err));
-	};
+    axios
+      .post("https://travel-game-python.herokuapp.com/api/registration/", {
+        username: data.username,
+        password1: data.password,
+        password2: data.password
+      })
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem("key", res.data.key);
+      })
+      .catch(err => console.log(err));
+  };
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input name="username" defaultValue="test" ref={register} />
-			<br />
-			<input name="password" ref={register({ required: true })} />
-			{errors.usernameRequired && <span>This field is required</span>}
-			<br />
-			<input type="submit" />
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="username" defaultValue="test" ref={register} />
+      <br />
+      <input name="password1" ref={register({ required: true })} />
+      {errors.usernameRequired && <span>This field is required</span>}
+      <br />
+      <input name="password2" ref={register({ required: true })} />
+      {errors.usernameRequired && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Register;
