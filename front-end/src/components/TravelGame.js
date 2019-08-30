7d1c769cb65726ab8d967c85736c118408d9ced4
Mario Amaya <thefozcat@gmail.com>
2019-08-29 20:56:43 -0700

    added Map and styles

--- front-end/src/components/TravelGame.js
+++ front-end/src/components/TravelGame.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 
 // Import Components
 
 import Controls from './Controls';
+import Map from './Map';
+
+// Styled Components
+
+const PageWrap = styled.div`
+  margin: 0 auto;
+`
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-weight: 600;
+  font-size: 1rem;
+  height: 1.4rem;
+`
+
+// TravelGame Component
 
 const TravelGame = () => {
   const [start, setStart] = useState();
@@ -52,22 +69,22 @@ const TravelGame = () => {
   console.log("start", start);
   console.log("token", "Token " + key);
   return (
-    <div>
+    <PageWrap>
+      <Map currentRoom={!start ? (null) : (start.title)}/>
+      <Controls playerMoves={playerMoves}/>
       {!start ? (
         <h3>Loading Player..</h3>
       ) : (
         <div>
-          <h2>Current Player</h2>
-          <h3>{start.name}</h3>
-          <p>{start.location}</p>
+          <h3>Current Player: {start.name}</h3>
+          <h3>Current Room: {start.title}</h3>
           <p>{start.description}</p>
         </div>
       )}
-      {/* {start.error_msg && (
-        <p className="error-msg">{start.error_msg}</p>
-      )} */}
-      <Controls playerMoves={playerMoves}/>
-    </div>
+      {!start ? (
+        null
+      ) : (<ErrorMessage>{start.error_msg}</ErrorMessage>)}
+    </PageWrap>
   );
 };
 export default TravelGame;
