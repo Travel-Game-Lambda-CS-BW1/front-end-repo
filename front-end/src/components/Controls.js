edf09cdea26ea17e71564377fd31515ccba6137c
Mario Amaya <thefozcat@gmail.com>
2019-08-29 17:39:17 -0700

    styled controls

--- front-end/src/components/Controls.js
+++ front-end/src/components/Controls.js
@@ -1,14 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// Styled Components
+
+const ControlButtons = styled.div`
+    display: grid;
+    grid-template-areas:
+        ". north ."
+        "west . east"
+        ". south .";
+    grid-gap: 5px;
+    margin: auto;
+    width: 100px;
+    background: lightgrey;
+
+    button {
+        width: 30px;
+        height: 30px;
+        background: grey;
+        border: none;
+        font-weight: 600;
+        cursor: pointer;
+
+        &:hover {
+            background: black;
+            color: green;
+            transition: all 200ms ease;
+        }
+    }
+
+    .north {
+        grid-area: north;
+    }
+
+    .east {
+        grid-area: east;
+    }
+
+    .south {
+        grid-area: south;
+    }
+
+    .west {
+        grid-area: west;
+    }
+
+    .space {
+        grid-area: space;
+        width: 30px;
+        height: 30px;
+    }
+`
+
+// Controls Component
 
 const Controls = props => {
 
     return (
-        <div className="control-buttons">
-            <button onClick={() => props.playerMoves('n')}>N</button>
-            <button onClick={() => props.playerMoves('e')}>E</button>
-            <button onClick={() => props.playerMoves('s')}>S</button>
-            <button onClick={() => props.playerMoves('w')}>W</button>
-        </div>
+        <ControlButtons>
+            <button onClick={() => props.playerMoves('n')} className="north">N</button>
+            <button onClick={() => props.playerMoves('e')} className="east">E</button>
+            <button onClick={() => props.playerMoves('s')} className="south">S</button>
+            <button onClick={() => props.playerMoves('w')} className="west">W</button>
+        </ControlButtons>
     )
 }
 
